dfb9585ce7d292ac0a03ab16a37463e9f0efd6c9
naharweb15 <naharweb15@gmail.com>
2024-01-16 19:14:26 +0600

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -151,13 +151,13 @@ document.addEventListener('DOMContentLoaded', function() {
 ///---- Scroll to Top end---///
 
 // sound wave js start///
-for(let i = 0; i < 10; i++){
-  
+for (let i = 0; i < 10; i++) {
   const left = (i * 2) + 1;
-  const anim = Math.floor(Math.random() * 75 + 400);
+  const anim = Math.floor(Math.random() * 600 + 600); // Adjust the range for slower animations
   const height = Math.floor(Math.random() * 25 + 3);
   console.log(height);
-  
-  document.querySelector('#bars').innerHTML += `<div class="bar" style="left:${left}px;animation-duration:${anim}ms;height:${height}px"></div>`;//`<div class="bar" style="left:${left}px">Hello</div>`;
+
+  document.querySelector('#bars').innerHTML += `<div class="bar" style="left:${left}px;animation-duration:${anim}ms;height:${height}px"></div>`;
 }
+
 // sound wave js end///
