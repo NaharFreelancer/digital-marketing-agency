8d826d23db5dfeaa671a62dcf15b5930b60bb0ef
naharweb15 <naharweb15@gmail.com>
2024-01-19 10:50:40 +0600

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -162,7 +162,7 @@ for (let i = 0; i < 10; i++) {
 
 // sound wave js end///
 
-
+// accordion js start///
 document.addEventListener("DOMContentLoaded", function () {
   var accordIcons = document.querySelectorAll('.accord-icon');
 
@@ -174,3 +174,28 @@ document.addEventListener("DOMContentLoaded", function () {
       });
   });
 });
+// accordion js end///
+
+// pagination js start///
+
+// jQuery Plugin: http://flaviusmatis.github.io/simplePagination.js/
+
+var items = $(".list-wrapper .list-item");
+    var numItems = items.length;
+    var perPage = 4;
+
+    items.slice(perPage).hide();
+
+    $('#pagination-container').pagination({
+        items: numItems,
+        itemsOnPage: perPage,
+        prevText: "&laquo;",
+        nextText: "&raquo;",
+        onPageClick: function (pageNumber) {
+            var showFrom = perPage * (pageNumber - 1);
+            var showTo = showFrom + perPage;
+            items.hide().slice(showFrom, showTo).show();
+        }
+    });
+// pagination js end///
+
